fb74fd68cdeb17a93447b468dc3ef86d67c30f44
Apurvvats <apurvvts@gmail.com>
2025-08-21 20:52:19 +0530

    yml

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,6 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads")); // Serve static files if needed
