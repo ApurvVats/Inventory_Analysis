638c28b5db87a722bca16a730384c0134387784c
Apurvvats <apurvvts@gmail.com>
2025-09-04 23:40:37 +0530

    pm2 edit

--- Backend/ecosystem.config.js
+++ Backend/ecosystem.config.js
@@ -1,12 +1,20 @@
-module.exports = {
+// ecosystem.config.js
+import dotenv from 'dotenv';
+dotenv.config(); // Load env variables for PM2
+
+export default {
   apps: [
     {
       name: 'web-server',
       script: './server.js',
       instances: 1,
       exec_mode: 'cluster',
+      node_args: '--experimental-specifier-resolution=node',
       env: {
         NODE_ENV: 'production',
+        DATABASE_URL: process.env.DATABASE_URL,
+        CLIENT_URL: process.env.CLIENT_URL,
+        PORT: process.env.PORT || 4000,
       },
     },
     {
@@ -14,10 +22,13 @@ module.exports = {
       script: './workers/demandAnalysisWorker.js',
       instances: 1,
       exec_mode: 'fork',
-      autorestart: true,   
-      max_restarts: 10,   
+      autorestart: true,
+      max_restarts: 10,
+      node_args: '--experimental-specifier-resolution=node',
       env: {
         NODE_ENV: 'production',
+        DATABASE_URL: process.env.DATABASE_URL,
+        CLIENT_URL: process.env.CLIENT_URL,
       },
     },
   ],
