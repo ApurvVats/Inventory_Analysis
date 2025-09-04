096aceef94ac90ab1d5fff5f5635e6e5ca8c1bfc
Apurvvats <apurvvts@gmail.com>
2025-09-04 20:14:27 +0530

    Add pm2 configuration for worker process

---
+++ Backend/ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'web-server',
+      script: './server.js',
+      instances: 1,
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'demand-worker',
+      script: './workers/demandAnalysisWorker.js',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
